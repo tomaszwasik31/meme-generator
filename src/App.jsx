86e216b13269738a8910714e4ee2958a4dd59b1f
Tomasz Wasik <tomaszwasik31@gmail.com>
2023-02-07 16:26:40 +0000

    removed node_modules folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Meme from "./components/Meme";
 
 function App() {
   return (
-  <Header/>
+    <div>
+      <Header />
+      <Meme />
+    </div>
   );
 }
 
