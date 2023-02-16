e5fc7c22ffe1e0dd2bccb35a29ffab6ffe8d7f65
Tomasz Wasik <tomaszwasik31@gmail.com>
2023-02-16 13:46:34 +0000

    structure for exercise

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,11 @@
 import React from "react";
 import logo from "../img/troll-face.png";
-
 export default function Header() {
   return (
     <header className="header">
-      <img className="logo" src={logo} />
-      <h2>Header component</h2>
-      <h4>React Course - Project 3</h4>
+      <img src={logo} className="header--image" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
     </header>
   );
 }
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,46 @@
 import React from "react";
+import memesData from "../memesData.js";
 
 export default function Meme() {
+  /**
+   * Challenge:
+   * 1. Set up the text inputs to save to
+   *    the `topText` and `bottomText` state variables.
+   * 2. Replace the hard-coded text on the image with
+   *    the text being saved to state.
+   */
+
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
+  function getMemeImage() {
+    const memesArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
+
   return (
-    <div className="meme--wrapper">
-      <div className="meme--form">
-        <input className="meme--input top"type="txt" />
-        <input className="meme--input bottom"type="txt" />
+    <main>
+      <div className="form">
+        <input type="text" placeholder="Top text" className="form--input" />
+        <input type="text" placeholder="Bottom text" className="form--input" />
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
       </div>
-      <button className="meme--btn"> Get a new meme image 🖼</button>
-    </div>
+      <div className="meme">
+        <img src={meme.randomImage} className="meme--image" />
+        <h2 className="meme--text top">One does not simply</h2>
+        <h2 className="meme--text bottom">Walk into Mordor</h2>
+      </div>
+    </main>
   );
 }
