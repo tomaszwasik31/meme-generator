22904eeb145a96639223e3b0d6a995bb346b5cf4
tomaszwasik31 <80346266+tomaszwasik31@users.noreply.github.com>
2023-05-16 12:50:42 +0100

    gh-pages fix

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -58,7 +58,7 @@ export default function Meme() {
         </button>
       </div>
       <div className="meme">
-        <img src={meme.randomImage} className="meme--image" />
+        <img src={meme.randomImage} className="meme--image" alt="meme-img"/>
         <h2 className="meme--text top">{meme.topText}</h2>
         <h2 className="meme--text bottom">{meme.bottomText}</h2>
       </div>
